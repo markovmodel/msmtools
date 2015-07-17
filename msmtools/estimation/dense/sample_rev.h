1a8391e3cb3fb3bb2472adcac3cf3f61f7cb1f7c
marscher <m.scherer@fu-berlin.de>
2015-07-17 19:34:08 +0200

    [estimation/dense/tmatrix_sampler] fix #32. Use macros to catch calls to c99 functions, which are not available in MSVC.

--- msmtools/estimation/dense/sample_rev.h
+++ msmtools/estimation/dense/sample_rev.h
@@ -25,10 +25,8 @@
 
 /* * moduleauthor:: F. Noe <frank DOT noe AT fu-berlin DOT de>
  */
-#include <stdbool.h>
 
-double _square(double x);
-bool _is_positive(double x);
+int _is_positive(double x);
 double _update_step(double v0, double v1, double v2, double c0, double c1, double c2, int random_walk_stepsize);
 double _sum_all(double* X, int n);
 void _normalize_all(double* X, int n);
@@ -39,3 +37,9 @@ void _update_sparse_sparse(double* C, double* sumC, double* X, double* sumX, int
 
 void _print_matrix(double* X, int n);
 void _update_sparse_speedtest(double* C, double* sumC, double* X, double* sumX, int* I, int* J, int n, int n_idx, int n_step);
+
+
+// declarations
+void _update_sparse(double* C, double* sumC, double* X, double* sumX, int* I, int* J, int n, int n_idx, int n_step);
+void _generate_row_indexes(int* I, int n, int n_idx, int* row_indexes);
+void _normalize_all(double* X, int n);
