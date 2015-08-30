b1b855397f1bfccfb9bb6ab7b323a9eb69824264
marscher <m.scherer@fu-berlin.de>
2015-08-30 21:31:19 +0200

    partially added lgpl...

--- msmtools/analysis/dense/decomposition.py
+++ msmtools/analysis/dense/decomposition.py
@@ -1,3 +1,21 @@
+
+# This file is part of MSMTools.
+#
+# Copyright (c) 2015, 2014 Computational Molecular Biology Group
+#
+# MSMTools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 r"""This module provides matrix-decomposition based functions for the
 analysis of stochastic matrices
 
@@ -526,4 +544,3 @@ def timescales_from_eigenvalues(evals, tau=1):
     ts[np.logical_not(ind_abs_one)] = \
         -1.0 * tau / np.log(np.abs(evals[np.logical_not(ind_abs_one)]))
     return ts
-
