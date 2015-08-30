5516527a6c64b5c9ad62c86f564d2b0317410cbb
marscher <m.scherer@fu-berlin.de>
2015-08-30 21:37:22 +0200

    added newline after bsd lic

--- msmtools/util/matrix/matrix.py
+++ msmtools/util/matrix/matrix.py
@@ -22,6 +22,7 @@
 # ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 r"""Implementation of IO for dense and sparse matrices
 
 .. moduleauthor:: B.Trendelkamp-Schroer <benjamin DOT trendelkamp-schroer AT fu-berlin DOT de>
