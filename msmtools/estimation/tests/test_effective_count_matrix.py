bb599fa81886f66b303351091896b4b2f554c9a7
marscher <m.scherer@fu-berlin.de>
2015-11-18 16:51:49 +0100

    added test with comparision to a pre-computed result

--- msmtools/estimation/tests/test_effective_count_matrix.py
+++ msmtools/estimation/tests/test_effective_count_matrix.py
@@ -27,8 +27,8 @@ from msmtools.estimation import count_matrix, effective_count_matrix
 """Unit tests for the transition_matrix module"""
 
 
-
 class TestEffectiveCountMatrix(unittest.TestCase):
+
     def setUp(self):
         testpath = abspath(join(abspath(__file__), pardir)) + '/testfiles/'
         self.dtraj_long = np.loadtxt(testpath + 'dtraj.dat', dtype=int)
@@ -51,7 +51,7 @@ class TestEffectiveCountMatrix(unittest.TestCase):
         assert np.all(Ceff.toarray() <= C.toarray())
 
     def test_multitraj(self):
-        dtrajs = [[1,0,1,0,1,1,0,0,0,1], [2], [0,1,0,1]]
+        dtrajs = [[1, 0, 1, 0, 1, 1, 0, 0, 0, 1], [2], [0, 1, 0, 1]]
         # lag 1
         C = count_matrix(dtrajs, 1)
         Ceff = effective_count_matrix(dtrajs, 1)
@@ -64,6 +64,32 @@ class TestEffectiveCountMatrix(unittest.TestCase):
         assert np.array_equal(Ceff.shape, C.shape)
         assert np.array_equal(C.nonzero(), Ceff.nonzero())
         assert np.all(Ceff.toarray() <= C.toarray())
+
+
+class TestEffectiveCountMatrix_old_impl(unittest.TestCase):
+
+    def test_compare_with_old_impl(self):
+        # generated with v1.1@ from
+        # pyemma.datasets.load_2well_discrete().dtraj_T100K_dt10_n6good
+        Ceff_ref = np.array([[2.21353316e+04,   2.13659736e+03,   4.63558176e+02,
+                              1.56043628e+02,   3.88680098e+01,   1.14317676e+01],
+                             [1.84456322e+03,   3.74107190e+02,   1.79811199e+02,
+                              9.29024530e+01,   5.59412620e+01,   2.59727288e+01],
+                             [3.45678646e+02,   1.42148228e+02,   8.19775293e+01,
+                              7.75353971e+01,   5.73438875e+01,   8.19775293e+01],
+                             [9.08206988e+01,   6.53466003e+01,   7.82682445e+01,
+                              7.71606750e+01,   8.38060919e+01,   2.84276171e+02],
+                             [3.56219388e+01,   3.43186971e+01,   7.64568442e+01,
+                              1.13816439e+02,   2.51960055e+02,   1.33451946e+03],
+                             [1.57044024e+01,   3.26168358e+01,   1.12346879e+02,
+                              4.34287128e+02,   1.88573632e+03,   2.35837843e+04]])
+        import pkg_resources
+        f = pkg_resources.resource_filename('msmtools.estimation', 'tests/testfiles/dwell.npz')
+        ref_dtraj = np.load(f)['dtraj_T100K_dt10_n6good']
+        Ceff = effective_count_matrix(ref_dtraj, lag=10, average='row', mact=1.0).toarray()
+
+        np.testing.assert_allclose(Ceff, Ceff_ref, atol=1e-15, rtol=1e-8)
+
 
 if __name__ == "__main__":
     unittest.main()
