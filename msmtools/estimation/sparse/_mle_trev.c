187097dd57e6d42b4c5cdd67eea79a0cbd2a21a1
jouno <fab@physik.tu-berlin.de>
2015-12-17 23:22:40 +0100

    [estimation] optimized reversible MSM estimator

--- msmtools/estimation/sparse/_mle_trev.c
+++ msmtools/estimation/sparse/_mle_trev.c
@@ -19,7 +19,6 @@
 /* moduleauthor:: F. Paul <fabian DOT paul AT fu-berlin DOT de> */
 #include <stdlib.h>
 #include <math.h>
-//#include <string.h>
 #undef NDEBUG
 #include <assert.h>
 #include "../../util/sigint_handler.h"
@@ -54,98 +53,89 @@ int _mle_trev_sparse(double * const T_data, const double * const CCt_data,
 					const int * const i_indices, const int * const j_indices,
 					const int len_CCt, const double * const sum_C,
 					const int dim, const double maxerr, const int maxiter,
-//					double * const mu,
+/* 					double * const mu,*/
 					double eps_mu)
 {
   double rel_err;
   int i, j, t, err, iteration;
-  double *x, *x_new, *sum_x, *sum_x_old, *temp;
-  double CCt_ij;
+  double *sum_x, *sum_x_new, *temp;
+  double CCt_ij, value;
   double x_norm;
 
   sigint_on();
 
   err = 0;
 
-  x = (double*)malloc(len_CCt*sizeof(double));
-  x_new= (double*)malloc(len_CCt*sizeof(double));
   sum_x= (double*)malloc(dim*sizeof(double));
-  sum_x_old= (double*)malloc(dim*sizeof(double));
-  if(!(x && x_new && sum_x && sum_x_old)) { err=1; goto error; }
+  sum_x_new= (double*)malloc(dim*sizeof(double));
+  if(!(sum_x && sum_x_new)) { err=1; goto error; }
 
   /* ckeck sum_C */
   for(i = 0; i<dim; i++) if(sum_C[i]==0) { err=3; goto error; }
 
-  /* initialize x */
+  /* initialize sum_x_new */
   x_norm = 0;
-  for(t = 0; t<len_CCt; t++) x_norm += CCt_data[t];
-  for(t = 0; t<len_CCt; t++) x_new[t]= CCt_data[t]/x_norm;
-
-  /* initialize sum_x */
-  for(i = 0; i<dim; i++) sum_x[i] = 0;
+  for(i=0; i<dim; i++) sum_x_new[i]=0;
+  for(t=0; t<len_CCt; t++) {
+      j = j_indices[t];
+      CCt_ij = CCt_data[t];
+      sum_x_new[j] += CCt_ij;
+      x_norm += CCt_ij;
+  }
+  for(i=0; i<dim; i++) sum_x_new[i] /= x_norm;
 
   /* iterate */
   iteration = 0;
   do {
     /* swap buffers */
-    temp = x;
-    x = x_new;
-    x_new = temp;
-
     temp = sum_x;
-    sum_x = sum_x_old;
-    sum_x_old = temp;
+    sum_x = sum_x_new;
+    sum_x_new = temp;
 
-    /* update x_sum */
-    for(i = 0; i<dim; i++) sum_x[i] = 0;
-    for(t = 0; t<len_CCt; t++) { 
-      j = j_indices[t];
-      sum_x[j] += x[t];
-    }
-    for(i = 0; i<dim; i++) if(sum_x[i]==0 || isnan(sum_x[i])) { err=2; goto error; }
-
-    /* update x */
-    x_norm = 0;
+    /* update sum_x */
+    for(i=0; i<dim; i++) sum_x_new[i] = 0;
     for(t=0; t<len_CCt; t++) {
       i = i_indices[t];
       j = j_indices[t];
       CCt_ij = CCt_data[t];
-      x_new[t] = CCt_ij / (sum_C[i]/sum_x[i] + sum_C[j]/sum_x[j]);
-      x_norm += x_new[t];
+      value = CCt_ij / (sum_C[i]/sum_x[i] + sum_C[j]/sum_x[j]);
+      sum_x_new[j] += value;
     }
 
-    /* normalize x */
-    for(t=0; t<len_CCt; t++) {
-      x_new[t] /= x_norm;
-      if (x_new[t] <= eps_mu) { err = 6; goto error; }
+    for(i = 0; i<dim; i++) if(sum_x_new[i]==0 || isnan(sum_x_new[i])) { err=2; goto error; }
+
+    /* normalize sum_x */
+    x_norm = 0;
+    for(i=0; i<dim; i++) x_norm += sum_x_new[i];
+    for(i=0; i<dim; i++) {
+      sum_x_new[i] /= x_norm;
+      if (sum_x_new[i] <= eps_mu) { err = 6; goto error; }
     }
 
     iteration += 1;
-    rel_err = relative_error(dim, sum_x, sum_x_old);
+    rel_err = relative_error(dim, sum_x, sum_x_new);
   } while(rel_err > maxerr && iteration < maxiter && !interrupted);
 
   /* calculate T */
   for(t=0; t<len_CCt; t++) {
-      i = i_indices[t];
-      j = j_indices[t];
-      T_data[t] = x[t] / sum_x[i];
+    i = i_indices[t];
+    j = j_indices[t];
+    CCt_ij = CCt_data[t];
+    value = CCt_ij / (sum_C[i]/sum_x_new[i] + sum_C[j]/sum_x_new[j]);
+    T_data[t] = value / sum_x_new[i];
   }
 
   if(iteration==maxiter) { err=5; goto error; }
 
-  //memcpy(mu, x_new, len_CCt*sizeof(double));
-  free(x);
-  free(x_new);
-  free(sum_x_old);
+  /*memcpy(mu, x_new, len_CCt*sizeof(double));*/
+  free(sum_x_new);
   free(sum_x);
   sigint_off();
   return 0;
 
 error:
-  //memcpy(mu, x_new, len_CCt*sizeof(double));
-  free(x);
-  free(x_new);
-  free(sum_x_old);
+  /*memcpy(mu, x_new, len_CCt*sizeof(double));*/
+  free(sum_x_new);
   free(sum_x);
   sigint_off();
   return -err;
