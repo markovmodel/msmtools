c678b3b1f3cd07528d84524188ff06e8182b82ca
Paul, Fabian (fapa) <fabian.paul@mpikg.mpg.de>
2016-08-19 15:32:59 +0200

    [rate matrix] removed ols unit test of own exprel implementation

--- msmtools/estimation/tests/test_ratematrix.py
+++ msmtools/estimation/tests/test_ratematrix.py
@@ -16,39 +16,6 @@ class TestLowlevelNumerics(unittest.TestCase):
        b = np.random.randn(d1,d2)
        assert np.allclose(a.dot(b), kahandot.kdot(a,b))
 
-    def test_exprel(self):
-        from decimal import Decimal, getcontext
-        step_size = 0.9 #0.1
-        digits = 100
-        getcontext().prec = 2*digits
-        X = np.empty((1,1),dtype=np.float64)
-        # test negative values
-        for i in np.arange(-digits,digits,step=step_size):
-            # reference
-            d = -(Decimal(10)**Decimal(i))
-            reference = float((d.exp()-Decimal(1))/d)
-            # tested value
-            X[0,0] = -(10.0**i)
-            tested1 = kahandot.exprel(X)[0,0]
-            tested2 = kahandot.exprel2(X)[0,0]
-            #print i, reference, tested1
-            assert np.allclose(reference, tested1)
-            assert np.allclose(reference, tested2)
-        # test 0
-        X[0,0] = 0.0
-        assert kahandot.exprel(X)[0,0]==1.0
-        assert kahandot.exprel2(X)[0,0]==1.0
-        # test positive values
-        maxexp = np.log(getcontext().Emax*np.log(10))/np.log(10)
-        for i in np.arange(-digits,maxexp,step=step_size):
-            d = (Decimal(10)**Decimal(i))
-            reference = float((d.exp()-Decimal(1))/d)
-            X[0,0] = (10.0**i)
-            tested1 = kahandot.exprel(X)[0,0]
-            tested2 = kahandot.exprel2(X)[0,0]
-            assert np.allclose(reference, tested1)
-            assert np.allclose(reference, tested2)
-
 class TestEstimators(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
