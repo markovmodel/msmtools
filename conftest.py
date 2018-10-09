0593fd266a9430763dd83cc5b433cbb5bbaff874
marscher <m.scherer@fu-berlin.de>
2018-10-09 17:40:58 +0200

    show pending deprecation warning only once

--- conftest.py
+++ conftest.py
@@ -5,3 +5,4 @@ import pytest
 @pytest.fixture(scope='session', autouse=True)
 def session_setup():
     warnings.filterwarnings('once', category=DeprecationWarning)
+    warnings.filterwarnings('once', category=PendingDeprecationWarning)
