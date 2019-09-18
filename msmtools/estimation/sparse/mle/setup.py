4fc9ea458be0ce9e488a486e64569643536d86a3
marscher <m.scherer@fu-berlin.de>
2019-09-18 16:45:08 +0200

    fix include path

--- msmtools/estimation/sparse/mle/setup.py
+++ msmtools/estimation/sparse/mle/setup.py
@@ -5,14 +5,13 @@ import os
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('mle', parent_package, top_path)
-    common_include_dir = os.path.join(config.path_in_package, '..', '..', '..', 'util', 'include')
-
+    common_include_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                      '..', '..', '..', 'util', 'include'))
+    assert os.path.exists(common_include_dir)
     config.add_extension('mle_trev',
                          sources=['mle_trev.pyx',
-                                  'src/_mle_trev.c',
-                                  ],
-                         include_dirs=['src/', np.get_include(), common_include_dir]
-                         )
+                                  'src/_mle_trev.c'],
+                         include_dirs=['src/', np.get_include(), common_include_dir])
 
     config.add_extension('mle_trev_given_pi',
                          sources=['mle_trev_given_pi.pyx',
