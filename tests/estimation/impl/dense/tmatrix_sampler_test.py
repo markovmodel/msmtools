df908c6eb25095c0beab0734f9282726da862a32
marscher <m.scherer@fu-berlin.de>
2019-09-18 15:04:06 +0200

    minor

--- tests/estimation/impl/dense/tmatrix_sampler_test.py
+++ tests/estimation/impl/dense/tmatrix_sampler_test.py
@@ -1,4 +1,3 @@
-
 # This file is part of MSMTools.
 #
 # Copyright (c) 2015, 2014 Computational Molecular Biology Group, Freie Universitaet Berlin (GER)
@@ -35,18 +34,18 @@ class TestSamplerNonReversible(unittest.TestCase):
         """Store state of the rng"""
         self.state = np.random.mtrand.get_state()
 
-        """Reseed the rng to enforce 'deterministic' behavior"""
+        # Reseed the rng to enforce 'deterministic' behavior"
         np.random.mtrand.seed(42)
 
-        self.C = 1.0*np.array([[7048, 6, 2], [6, 2, 3], [2, 3, 2933]])
+        self.C = 1.0 * np.array([[7048, 6, 2], [6, 2, 3], [2, 3, 2933]])
 
-        """Mean in the asymptotic limit, N_samples -> \infty"""
+        # Mean in the asymptotic limit, N_samples -> \infty
         alpha = self.C
         alpha0 = self.C.sum(axis=1)
 
-        self.mean = alpha/alpha0[:, np.newaxis]
-        self.var = alpha * (alpha0[:,np.newaxis] - alpha)/\
-            (alpha0**2 * (alpha0 + 1.0))[:,np.newaxis]
+        self.mean = alpha / alpha0[:, np.newaxis]
+        self.var = alpha * (alpha0[:, np.newaxis] - alpha) / \
+                   (alpha0 ** 2 * (alpha0 + 1.0))[:, np.newaxis]
 
         self.N = 1000
 
@@ -58,25 +57,22 @@ class TestSamplerNonReversible(unittest.TestCase):
         """Create sampler object"""
         sampler = TransitionMatrixSampler(self.C, reversible=False)
 
-        """Compute sample mean"""
+        # Compute sample mean
         mean = np.zeros_like(self.C)
         for i in range(self.N):
             mean += sampler.sample()
-        mean *= 1.0/self.N
+        mean *= 1.0 / self.N
 
-        """Check if sample mean and true mean fall into the 2\sigma intervall"""
-        self.assertTrue(np.all( np.abs(mean - self.mean) <= 2.0*np.sqrt(self.var/self.N) ))
+        # Check if sample mean and true mean fall into the 2\sigma interval
+        self.assertTrue(np.all(np.abs(mean - self.mean) <= 2.0 * np.sqrt(self.var / self.N)))
 
 
 class TestSamplerReversible(unittest.TestCase):
 
     def setUp(self):
-        self.C = 1.0*np.array([[7048, 6, 0], [6, 2, 3], [0, 3, 2933]])
+        self.C = 1.0 * np.array([[7048, 6, 0], [6, 2, 3], [0, 3, 2933]])
         self.P_mle = tmatrix(self.C, reversible=True)
         self.N = 1000
-
-    def tearDown(self):
-        pass
 
     def test_mean(self):
         """Create sampler object"""
@@ -88,14 +84,15 @@ class TestSamplerReversible(unittest.TestCase):
         mean = np.mean(sample, axis=0)
         std = np.std(sample, axis=0)
 
-        """Check if sample mean and MLE agree within the sample standard deviation"""
-        self.assertTrue(np.all( np.abs(mean - self.P_mle) <= std) )
+        # Check if sample mean and MLE agree within the sample standard deviation
+        self.assertTrue(np.all(np.abs(mean - self.P_mle) <= std))
+
 
 class TestSamplerReversiblePi(unittest.TestCase):
 
     def setUp(self):
-        self.C = 1.0*np.array([[7048, 6, 0], [6, 2, 3], [0, 3, 2933]])
-        self.pi = np.array([ 0.70532947,  0.00109989,  0.29357064])
+        self.C = 1.0 * np.array([[7048, 6, 0], [6, 2, 3], [0, 3, 2933]])
+        self.pi = np.array([0.70532947, 0.00109989, 0.29357064])
         self.P_mle = tmatrix(self.C, reversible=True, mu=self.pi)
 
         self.N = 1000
@@ -113,8 +110,9 @@ class TestSamplerReversiblePi(unittest.TestCase):
         mean = np.mean(sample, axis=0)
         std = np.std(sample, axis=0)
 
-        """Check if sample mean and MLE agree within the sample standard deviation"""
-        self.assertTrue(np.all( np.abs(mean - self.P_mle) <= std) )
+        # Check if sample mean and MLE agree within the sample standard deviation
+        self.assertTrue(np.all(np.abs(mean - self.P_mle) <= std))
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     unittest.main()
