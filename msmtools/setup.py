ccdc6c47dd42c515752c8f0258cd4f5a3649d59a
marscher <m.scherer@fu-berlin.de>
2019-09-18 15:03:18 +0200

    numpy package config complete

--- msmtools/setup.py
+++ msmtools/setup.py
@@ -5,27 +5,23 @@ def configuration(parent_package='', top_path=None):
     config = Configuration('msmtools', parent_package, top_path)
 
     config.add_subpackage('analysis')
-    # TODO: slash or dot??
-    config.add_subpackage('analysis/dense')
-    config.add_subpackage('analysis/sparse')
+    config.add_subpackage('analysis.dense')
+    config.add_subpackage('analysis.sparse')
 
+    config.add_subpackage('dtraj')
     config.add_subpackage('estimation')
+
     config.add_subpackage('flux')
+    config.add_subpackage('flux.dense')
+    config.add_subpackage('flux.sparse')
+
     config.add_subpackage('generation')
     config.add_subpackage('util')
 
-    import numpy as np
-    import os
-    config.add_include_dirs(np.get_include())
-    config.add_include_dirs(os.path.abspath('util/include'))
-
-
     from Cython.Build import cythonize
-    import sys
     config.ext_modules = cythonize(
         config.ext_modules,
-        #compile_time_env={'SKLEARN_OPENMP_SUPPORTED': with_openmp},
-        compiler_directives={'language_level': sys.version_info[0]})
+        compiler_directives={'language_level': '3'})
 
     return config
 
