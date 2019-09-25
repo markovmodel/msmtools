ebd6b16f8f0d04865803f49d20bbaa45c0ca735d
marscher <m.scherer@fu-berlin.de>
2019-09-25 14:18:16 +0200

    fix imports

--- msmtools/estimation/dense/__init__.py
+++ msmtools/estimation/dense/__init__.py
@@ -21,5 +21,5 @@ from . import bootstrapping
 from . import transition_matrix
 from . import covariance
 
-#from .mle import mle_trev, mle_trev_given_pi
+from .mle import mle_trev, mle_trev_given_pi
 from .tmat_sampling import tmatrix_sampler
